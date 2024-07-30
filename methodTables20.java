1c922e4798cf4d9e3a8251b860d89190a893deff
dhanushgr <grdhanush8@gmail.com>
2024-07-30 12:09:32 +0530

    created Method() class created a method class for tables(1 to 20)

---
+++ methodTables20.java
@@ -0,0 +1,20 @@
+//Created a seperate class to generate tables(1 to 20)
+
+public class methodTables20 {
+
+    public static void main(String[] args) {
+        createTables20();           // method invoke OR method call
+    }
+    
+    // Method should not be defined in side the void main method because its already a method.
+    // we cant define a method inside other method. we can call/invoke a method inside ones class.
+    public static void createTables20(){
+        for (int table=1; table<=20; table++){
+            System.err.println();               // this line is for printing the gap after one whole table completed.
+            for(int i=1; i<=10; i++){
+                System.err.printf("%d * %d = %d",table, i, table*i ).println();
+            }
+        }
+    }
+
+}
