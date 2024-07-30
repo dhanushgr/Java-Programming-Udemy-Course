048cafb16bf89aa8c7802816b6d0eb0f47e3c1a0
dhanushgr <grdhanush8@gmail.com>
2024-07-30 11:32:56 +0530

    created tables created table which prints tables from 1 to 20 used for loop to use to print

---
+++ Table20.java
@@ -0,0 +1,12 @@
+//Printing 1 to 20 tables.
+
+public class Table20 {
+    public static void main(String args[]){
+        for (int table=1; table<=20; table++){
+            System.err.println();               // this line is for printing the gap after one whole table completed.
+            for(int i=1; i<=10; i++){
+                System.err.printf("%d * %d = %d",table, i, table*i ).println();
+            }
+        }
+    }
+}
