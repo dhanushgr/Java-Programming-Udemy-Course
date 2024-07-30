43243608919d4f64f9465b5ef1d5c79a746aafb0
dhanushgr <grdhanush8@gmail.com>
2024-07-30 16:40:03 +0530

    created methodWithParameter This method prints the statement as many times we want, we just need to mention the parameter in it. If we need to print the statement 20 times , pass the parameter as 20.

---
+++ methodWithParameter.java
@@ -0,0 +1,12 @@
+public class methodWithParameter {
+    public static void main(String args[]){
+        methodParameter(20);                       //method call/invoke, it prints as many times as i mention in the parameter
+    }
+    
+    public static void methodParameter(int noOfTimes){      //method with parameter(noOfTimes)
+        for(int i=1; i<=noOfTimes; i++)                     //for loop to print the content as many times as i want (that is as many noOfTimes)
+        {
+            System.out.println("printing the things"); 
+        }
+    }
+}
