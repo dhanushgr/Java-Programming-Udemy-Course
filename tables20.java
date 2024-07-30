d4839e287f743139c1dca7c3fd37b172519b79ac
dhanushgr <grdhanush8@gmail.com>
2024-07-30 12:07:55 +0530

    renamed file renamed: Table20.java --> tables20.java

--- tables20.java
+++ tables20.java
@@ -1,6 +1,6 @@
 //Printing 1 to 20 tables.
 
-public class Table20 {
+public class tables20 {
     public static void main(String args[]){
         for (int table=1; table<=20; table++){
             System.err.println();               // this line is for printing the gap after one whole table completed.
