096c335a48e8b171e6f489a3873eedb08ccf29d7
dhanushgr <grdhanush8@gmail.com>
2024-07-30 16:16:29 +0530

    modified System.err to system.out

--- tables20.java
+++ tables20.java
@@ -3,9 +3,9 @@
 public class tables20 {
     public static void main(String args[]){
         for (int table=1; table<=20; table++){
-            System.err.println();               // this line is for printing the gap after one whole table completed.
+            System.out.println();               // this line is for printing the gap after one whole table completed.
             for(int i=1; i<=10; i++){
-                System.err.printf("%d * %d = %d",table, i, table*i ).println();
+                System.out.printf("%d * %d = %d",table, i, table*i ).println();
             }
         }
     }
