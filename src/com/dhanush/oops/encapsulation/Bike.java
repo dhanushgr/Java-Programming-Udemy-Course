fb54fcde792e6f8c17d9f93ef38eddea080ecbc2
dhanushgr <grdhanush8@gmail.com>
2024-08-05 09:09:01 +0530

    created encapsulaiton files

--- src/com/dhanush/oops/encapsulation/Bike.java
+++ src/com/dhanush/oops/encapsulation/Bike.java
@@ -5,14 +5,39 @@ package com.dhanush.oops.encapsulation;
 public class Bike {
 
     // int speed;  --> this is instance variable
-    private int speed;  // --> this is MEMBER VARIABLE, private makes this field not accesable by other class. 
- 
+    private int speed;  // --> this is MEMBER VARIABLE, private makes this field not accesable by other class.
+
 /*to access this variable you should use a method. Which will eventually take inputs when the method is called in tha main method.
  * not allowing other classes to directly change the field value of this class is called encapsulation.
  * this Encapsulation allows this method to stay private and protects it by other classes(it encapsulated this class).
  */
+ 
 
-    int setSpeed(int speed){
-        return this.speed;      //same as "System.out.println(speed)"
+    //setter
+    void setSpeed(int speed){
+         //return this.speed;(if method is int, tred it wont print anything)      //same as "System.out.println(speed)"
+         this.speed = speed;        //It assings the value of the local variable to this class's variable
+         // System.out.println(speed);
+         // System.out.println(this.speed);
+        }
+        
+        //gettter
+        int getSpeed(){
+            return this.speed;
+                            //This is also useful to maintain the Abstraction property: write all the code at one class, and call the required class in the main class. The user dont need to deal with the logic of everything.
+                            //This type of methods are created so that in encapsulation this methods() can be called rather than assigning the data to the variable directly.
+    }
+
+
+
+    //Default Constructor
+    Bike(){
+        this(5);          //this calls the other constructor with same arguments as given. It calls Bike(int speed)         
+                                // OR this.speed=30;              
+                                //This Constructor could stay empty or you can set a default value when constructing.
+    }
+
+    Bike(int speed){            //This is a Constructor with arguments.
+        this.speed= speed;
     }
 }
--- src/com/dhanush/oops/encapsulation/BikeMain.java
+++ src/com/dhanush/oops/encapsulation/BikeMain.java
@@ -1,5 +1,17 @@
 package com.dhanush.oops.encapsulation;
 
 public class BikeMain {
-    
+    public static void main(String[] args) {
+        Bike honda = new Bike();
+        Bike ducati = new Bike(90);     //This will directly set the speed to 90 with out calling the obj.setSpeed() method.
+        Bike pulsar = new Bike();             //For this obj the speed will be directly set to 5. which is mention in the Bike class DefaultConstructor
+
+        honda.setSpeed(45);             //sets the speed of honda from 5 to 45
+
+        System.out.println(honda.getSpeed());
+        System.out.println(ducati.getSpeed());
+        System.out.println(pulsar.getSpeed());
+
+        
+    }
 }
