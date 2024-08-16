1a10ff1fe7aec3cd5534bc1ab2d4325c1ee96abb
dhanushgr <grdhanush8@gmail.com>
2024-08-16 19:38:15 +0530

    created ArrayMagic file added doesHaveElementGreaterThan method it checks if the given number is greater than the numbers which are present in the array

---
+++ src/com/dhanush/Exercise/Set6Arrays/ArrayMagicMain.java
@@ -0,0 +1,9 @@
+package src.com.dhanush.Exercise.Set6Arrays;
+
+public class ArrayMagicMain {
+    public static void main(String[] args) {
+        ArrayMagic arrayMagic = new ArrayMagic();
+        int[] array = {1,5,6,8,9};
+        System.out.println(arrayMagic.doesHaveElementGreaterThan(array, 4));
+    }
+}
