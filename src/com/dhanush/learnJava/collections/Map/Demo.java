608ba47d4ac94538de7acdb0bfb81a139cedcbcd
dhanushgr <grdhanush8@gmail.com>
2024-08-25 05:08:30 +0530

    created package Map                added demo.java: explains every type of Map                added Occurances problem's solutions

--- src/com/dhanush/learnJava/collections/Map/Demo.java
+++ src/com/dhanush/learnJava/collections/Map/Demo.java
@@ -1,9 +1,44 @@
 package src.com.dhanush.learnJava.collections.Map;
 
-import java.util.Map;
+import java.util.*;
 
-public class MapMain {
+public class Demo {
     public static void main(String[] args) {
-        Map<>
+
+        Map<String, Integer> map = Map.of("A",2,"B",3,"C",4,"D",5);
+        /*
+            you can insert the elements in 2 ways.
+                1st method insert the elements to the map by using .of() method
+                and then insert the map object to the hashMap as an argument
+                2nd method is directly insert the elements to hashMap by
+                .of() inside the argument, refer Queue for example
+         */
+
+        //HashMap
+        Map<String, Integer> hashMap = new HashMap<>(map);
+        System.out.println(hashMap);
+        System.out.println(hashMap.get("A"));
+        hashMap.put("E",6);
+        System.out.println(hashMap);
+        System.out.println(hashMap.keySet());
+        System.out.println(hashMap.values());
+        hashMap.clear();
+
+        //HashTable
+        Map<String, Integer> hashTable = new Hashtable<>(map);
+        System.out.println("hashTable --> "+hashTable);
+
+        //LinkedHashMap
+        Map<String, Integer> linkedhashMap = new LinkedHashMap<>(map);
+        System.out.println("LinkedHashMap --> "+linkedhashMap);
+        linkedhashMap.put("Z", 20);
+        System.out.println("LinkedHashMap --> "+linkedhashMap);
+
+        //TreeMap
+        Map<String, Integer> treeMap = new TreeMap<>(map);
+        System.out.println(STR."TreeMap --> \{treeMap}");
+        treeMap.put("Y",10);
+        System.out.println(STR."TreeMap --> \{treeMap}");
+
     }
 }
--- src/com/dhanush/learnJava/collections/Map/Occurances.java
+++ src/com/dhanush/learnJava/collections/Map/Occurances.java
@@ -1,4 +1,65 @@
+/*
+    Problem Statement:
+        How many occurances of the characters and words in a String with the help of HashMap
+ */
+
 package src.com.dhanush.learnJava.collections.Map;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Occurances {
+    public static void main(String[] args) {
+        String str = "This is an awesome occasion . This was the best occasion I had in years";
+
+        Map<Character, Integer> occurances = new HashMap<>();
+
+        /*
+            this is used to store the string in the form of characters
+            in a new array of type characters.
+            .toCharArray() converts the String to a character array
+         */
+        char[] characters = str.toCharArray();
+        System.out.println(characters.length);
+        System.out.println(characters);
+
+        for (char character : characters) {
+            /*
+                checks if the key(character(like A or B)) has any value in it.
+                .get(key): returns the value of that key. It will return an integer type
+             */
+            Integer integer = occurances.get(character);
+            /*
+                if there is no value present for that key that is null
+                (there is no key present, so should create a new key with the value)
+                here .put() creates a new key and value
+             */
+            if (integer == null) {
+                occurances.put(character, 1);   //sets the value as 1
+            } else {                    //if the key is present with some value
+                occurances.put(character, integer + 1);     //then increment that value by 1 (here .put() updates the value of the key to +1)
+            }
+        }
+
+        System.out.println(occurances);
+
+
+        //StringOccurances
+        Map<String, Integer> stringOccurances = new HashMap<>();
+
+        String[] words = str.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        for (String word : words) {
+            Integer integer = stringOccurances.get(word);
+            if (integer == null) {
+                stringOccurances.put(word, 1);
+            } else {
+                stringOccurances.put(word, integer + 1);
+            }
+        }
+
+        System.out.println(stringOccurances);
+    }
 }
