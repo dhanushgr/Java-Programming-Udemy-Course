5dd3ba1655baa046a62cba34e12a77cf0b108bb1
dhanushgr <grdhanush8@gmail.com>
2024-10-10 04:00:34 +0530

    updated and deleted some files in package FuntionalProgramming combined the code of SumOfNumbersFP.java into FunctionalProgrammingMain.java file deleted SumOfNumbersFP.java file updated FunctionalProgrammingMain.java file with Sorted, Distinct and Map methods

--- src/com/dhanush/learnJava/FunctionalProgramming/FunctionalProgrammingMain.java
+++ src/com/dhanush/learnJava/FunctionalProgramming/FunctionalProgrammingMain.java
@@ -9,10 +9,17 @@ import java.util.List;
 public class FunctionalProgrammingMain {
     public static void main(String[] args) {
 
+        /*
+            Lists which are going to be used in this program, as an
+            argument/parameter for some methods
+         */
         List<String> words = List.of("Apple", "Banana", "Cat", "Dog");
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        List<Integer> numbers2 = List.of(6, 3, 8, 2, 9, 8, 6);
         List<String> words2 = List.of("Apple", "Bat", "Cat", "Dog", "Eat");
+        //For .filter(), this String is used
 
+        //calling methods
         PrintBasic(words);
         System.out.println();
         PrintWithFP(words);
@@ -24,6 +31,23 @@ public class FunctionalProgrammingMain {
         PrintOddNumbers(numbers);
         System.out.println();
         PrintEvenNumbers(numbers);
+
+        /* all this statements are written because the fucntions have "Void"
+        as a return type, remaining functions which doesn't use sout
+        statements are using int/anyting as a return type */
+        System.out.println("Sum of numbers without FP"+ PrintSumOfNumbers(numbers));
+        System.out.println("Sum of numbers with FP" +PrintSumOfNumbersWithFP(numbers));
+        System.out.println("sum of even set of numbers is " +PrintSumOfEvenNumbers(numbers));
+        System.out.println(STR."sum of odd set of numbers is : \{PrintSumOfOddNumbers(numbers)}");
+
+        //calling methods
+        SortNumbers(numbers2);
+        System.out.println();
+        DistinctNumbers(numbers2);
+        System.out.println();
+        SortedAndDistinctNumbers(numbers2);
+        System.out.println();
+        PrintSquaresOfDistinctNumbers(numbers2);
 
     }
 
@@ -61,6 +85,69 @@ public class FunctionalProgrammingMain {
         numbers.stream()
                 .filter(element -> element%2 == 0)
                 .forEach(element ->System.out.println(element));
+    }
+
+    public static int PrintSumOfNumbers(List<Integer> numbers){
+        int sum = 0;
+        for(int number: numbers){
+            sum = sum + number;     //mutation: the value of sum is changing
+            // continuously, but in FP we avoid mutation of variable.
+        }
+        return sum;
+    }
+
+    private static int PrintSumOfNumbersWithFP(List<Integer> numbers) {
+        int sum = numbers.stream()
+                .reduce(0, (number1, number2) -> number1 + number2);
+        return sum;
+    }
+
+    /*
+        2+4+6
+    */
+    public static int PrintSumOfEvenNumbers(List<Integer> numbers){
+        int sum = numbers.stream()
+                .filter(element -> element%2 == 0)
+                .reduce(0, (number1, number2) -> number1 + number2 );
+        return sum;
+    }
+
+    /*
+        1+3+5
+    */
+    public static int PrintSumOfOddNumbers(List<Integer> numbers){
+        int sum = numbers.stream()
+                .filter(element -> element%2 == 1)
+                .reduce(0, (number1,number2) -> number1 + number2);
+        return sum;
+    }
+
+    public static void SortNumbers(List<Integer> numbers2){
+        numbers2.stream()
+                .sorted()
+                .forEach(element -> System.out.println(element));
+    }
+
+    public static void DistinctNumbers(List<Integer> numbers2){
+        numbers2.stream()
+                .distinct()
+                .forEach(element -> System.out.println(element));
+    }
+
+    public static void SortedAndDistinctNumbers(List<Integer> numbers2){
+        numbers2.stream()
+                .distinct()
+                .sorted()
+                .forEach(element -> System.out.println(element));
+    }
+
+    public static void PrintSquaresOfDistinctNumbers(List<Integer> numbers2){
+        numbers2.stream()
+                .distinct()
+                .map(e -> e*e)      //This is an intermediate function which
+                // squares the element, and then let it proceed with further
+                // function
+                .forEach(e -> System.out.println(e));
     }
 
 }
