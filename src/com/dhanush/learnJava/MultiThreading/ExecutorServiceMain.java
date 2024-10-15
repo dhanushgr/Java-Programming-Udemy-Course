3df416fbdc0505b333344001a568811aea08ca91
dhanushgr <grdhanush8@gmail.com>
2024-10-16 00:43:47 +0530

    updated ExecutorServiceMain added a new class with task constructor which can construct as many task i want implemented FixedThreadPool: which defines how many threads should run simultaneously

--- src/com/dhanush/learnJava/MultiThreading/ExecutorServiceMain.java
+++ src/com/dhanush/learnJava/MultiThreading/ExecutorServiceMain.java
@@ -8,12 +8,36 @@ public class ExecutorServiceMain {
     public static void main(String[] args) {
 
         // It only executes one Thread at a time
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        //ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        executorService.execute(new Task1());
-        executorService.execute(new Thread(new Task2()));
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+//        executorService.execute(new Task1());
+//        executorService.execute(new Thread(new Task2()));
+
+        executorService.execute(new Task(1));
+        executorService.execute(new Task(2));
+        executorService.execute(new Task(3));
+        executorService.execute(new Task(4));
 
         executorService.shutdown();
     }
 
+}
+
+//created a class with constructor for any number of tasks
+class Task extends Thread{
+    private int number;
+
+    public Task(int number){
+        this.number = number;
+    }
+
+    public void run(){
+        System.out.println("Task "+ number +"started");
+        for (int i = number*100; i<number*100 + 99; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println("\nTask"+ number +"done");
+    }
 }
