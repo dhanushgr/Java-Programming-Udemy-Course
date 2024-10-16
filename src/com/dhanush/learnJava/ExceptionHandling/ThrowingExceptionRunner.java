ae41ecee00367b90a5f82d6e77993819f6842e5c
dhanushgr <grdhanush8@gmail.com>
2024-10-17 01:22:25 +0530

    updated ThrowingExceptionRunner updated it with throwing Exception instead of RunTimeException

--- src/com/dhanush/learnJava/ExceptionHandling/ThrowingExceptionRunner.java
+++ src/com/dhanush/learnJava/ExceptionHandling/ThrowingExceptionRunner.java
@@ -12,9 +12,14 @@ class Amount{
     }
 
     //a method for adding amount
-    public void add(Amount that){
+    /*
+        Throwing an Exception instead of RunTimeException, it requires to add
+        "throws Exception" in the method because its Checked Exception and
+         an Unchecked Exception like RunTimeException
+     */
+    public void add(Amount that) throws Exception {
         if(!this.currency.equals(that.currency)){
-            throw new RuntimeException("Currencies don't match");
+            throw new Exception(STR."Currencies don't match \{this.currency} & \{that.currency}");
         }
         this.amount = this.amount + that.amount;
     }
@@ -27,9 +32,9 @@ class Amount{
 }
 
 public class ThrowingExceptionRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Amount amount1 = new Amount(10, "rs");
-        Amount amount2 = new Amount(20, "rs");
+        Amount amount2 = new Amount(20, "usd");
         amount1.add(amount2);
         System.out.println(amount1);
     }
