c9731e6594f83572904a0ee9293609de8b1248d4
dhanushgr <grdhanush8@gmail.com>
2024-10-19 17:38:25 +0530

    added package set10 and done 2 problems in it

--- src/com/dhanush/Exercise/Set10/EnumsDaysOfTheWeek.java
+++ src/com/dhanush/Exercise/Set10/EnumsDaysOfTheWeek.java
@@ -1,4 +1,34 @@
 package src.com.dhanush.Exercise.Set10;
 
 public class EnumsDaysOfTheWeek {
+    enum DaysOfWeek {
+        MONDAY("Monday"),
+        TUESDAY("Tuesday"),
+        WEDNESDAY("Wednesday"),
+        THURSDAY("Thursday"),
+        FRIDAY("Friday"),
+        SATURDAY("Saturday"),
+        SUNDAY("Sunday");
+
+        private String name;
+
+        DaysOfWeek(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            // Write your code here to return the name of the day
+            return this.name;
+        }
+
+        public boolean isWeekday() {
+            // Write your code here to check if this day is a weekday
+            return this!=SATURDAY && this!=SUNDAY;
+        }
+
+        public boolean isHoliday() {
+            // Write your code here to check if this day is a holiday
+            return !isWeekday();
+        }
+    }
 }
